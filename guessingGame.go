f862995e701f9729313f519008bb74a24eb9853e
jono mannion <johnmannion51@gmail.com>
2017-10-03 18:55:23 +0100

    added comments

--- guessingGame.go
+++ guessingGame.go
@@ -1,31 +1,37 @@
 
 package main
+//imports required for program
 import(
     "fmt"
     "math/rand"
     "time"
-)
+)//import
 
-//this generates random number between given range
+//this function will be called upon to generate random numbers
 func xrand(min, max int) int {
     rand.Seed(time.Now().Unix())
     return rand.Intn(max - min) + min
-}
+}//xrand
 
 func main() {
     var myname string
+	//calls xrand function to generate a random number between 1 - 6
     myrand := xrand(1, 6)
+	//declared variables for user input
     guessTaken := 0
     var guess int
-
+	//prompts the user to enter a name and explains the rules of the game
     fmt.Println("Hello! What is your name?")
     fmt.Scanf("%s", &myname)
-    fmt.Printf("Well, %s, I am thinking of a number between 1 and 6.\n", myname)
+    fmt.Printf("Hello, %s, I am thinking of a number between 1 and 6.\n", myname)
     
-    //this is the while loop
+    //this is the for loop used to prompt the user to take a guess 
+	//if the users guess is too low they are prompted to have another go
+	//if the users guess is too hign they are prompted to have another go
+
     for guessTaken < 6 {
-        fmt.Println("Take a guess...")
-        fmt.Scanf("%d", &guess)
+        fmt.Println("Have a guess...")
+        fmt.Scanf("%d\n", &guess)
         guessTaken++
         if guess < myrand {
             fmt.Println("Your guess is too low.")
@@ -37,11 +43,16 @@ func main() {
             break
         }
     }
+	//informs the user if they have guessed correctly or 
+	//lets them know what the number was if they guessed incorrectly
     if guess == myrand {
-        fmt.Printf("Good job %s! You guessed my number in %d tries\n", myname, guessTaken)
+        fmt.Printf("Good job %s! You guessed my number was %d in %d tries\n", myname, myrand, guessTaken)
     } else {
-        fmt.Printf("Nope. The number I had in mind was %d\n", myrand)
+        fmt.Printf("Hard luck %s. The number I had in mind was %d\n", myname, myrand)
     }
-}
+}//main
 //source of code
 //http://golangcookbook.blogspot.ie/2012/11/guess-number-game-in-golang.html
+
+//problem with user input
+//https://www.socketloop.com/tutorials/golang-number-guessing-game-with-user-input-verification-example
