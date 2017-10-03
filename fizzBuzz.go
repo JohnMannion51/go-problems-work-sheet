1f6833d3b98a0e02790ae2df052b8b640d59d839
jono mannion <johnmannion51@gmail.com>
2017-10-03 18:54:45 +0100

    added comments

--- fizzBuzz.go
+++ fizzBuzz.go
@@ -1,21 +1,21 @@
 package main
 
 import "fmt"
-
+//use for loop to run from 1 - 100
 func main() {
     for i := 1; i <= 100; i++ {
-
+	
         if i%3 == 0 {
-            // Multiple of 3
+            // Multiples of 3 are viewed as fizz
             fmt.Printf("fizz")
         }
         if i%5 == 0 {
-            // Multiple of 5
+            // Multiples of 5 are viewed as buzz
             fmt.Printf("buzz")
         }
 
         if i%3 != 0 && i%5 != 0 {
-            // Neither, so print the number itself
+            // Multiples of both 3 and 5 are viewed as fizzbuzz
             fmt.Printf("%d", i)
         }
 
@@ -24,3 +24,5 @@ func main() {
 
     }
 }
+//source of code
+//https://golangcode.com/fizz-buzz-test-in-go/
